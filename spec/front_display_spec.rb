7ea3b153117ac4c8bc9764647c5c6cf96051ba84
ansonmela <anson.melakayil@yahoo.com>
2018-05-02 12:10:27 -0400

    added more tests to front_display, green state.

--- spec/front_display_spec.rb
+++ spec/front_display_spec.rb
@@ -15,5 +15,13 @@ describe Front_Display do
 		it "should show the price of cola on the display" do 
 			expect(front_display.show_price(1.00)).to eq("PRICE: 1.00")
 		end
+
+		it "should show the price of candy on the display" do 
+			expect(front_display.show_price(0.65)).to eq("PRICE: 0.65")
+		end
+
+		it "should show the price of chips on the display" do 
+			expect(front_display.show_price(0.50)).to eq("PRICE: 0.50")
+		end
 	end
 end
