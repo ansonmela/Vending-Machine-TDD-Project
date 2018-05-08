3acb20eca4e22d2ccf82d3b10fcca3b15bc98851
ansonmela <anson.melakayil@yahoo.com>
2018-05-08 13:44:29 -0400

    created vending machine class and vending_machine can now initialize other components. green state.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,7 +1,15 @@
+require 'coinmech'
+require 'products'
+require 'front_display'
+
 class Vending_Machine
 
 
-
+	def initialize
+		@coin_mech = CoinMech.new
+		@products = Products.new
+		@front_display = Front_Display.new
+	end
 
 
 
---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'vending_machine'
+
+describe Vending_Machine do 
+	let(:vending_machine) {Vending_Machine.new}
+	let(:coins) {Coins.new}
+
+	
+end
+
+			
+
